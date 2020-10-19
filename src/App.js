619d6a9778bb9964806944341d64afb7e56dc7a0
Mukul <65353419+mukulsaini22@users.noreply.github.com>
2020-10-20 00:12:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios'
+import './App.css';
+
+function App() {
+
+ const [Comments,setComments] = useState({})
+ const [Id,setId] =useState(1)
+
+ useEffect(()=>{
+
+  axios.get(`https://jsonplaceholder.typicode.com/posts/${Id}`).then(res=>
+  setComments(res.data)).catch(err=>console.log(err))
+ },[Id])
+
+
+  return (
+    <div className="App">
+      
+
+    <h1>
+  enter id
+
+    </h1>
+     <input   type='text' value={Id} onChange={e=>setId(e.target.value)} />   
+     <div>{Comments.title}</div>
+     
+     </div>
+  );
+}
+
+export default App;
